7da442058024e12ca09418ed7ed5eacb23a134ca
sanketpatil216 <45464462+sanketpatil216@users.noreply.github.com>
2021-04-13 14:42:06 -0600

    Update ip_address_switch.py

--- ip_address_switch.py
+++ ip_address_switch.py
@@ -15,22 +15,28 @@ for line_f1 in f1:                #Comparing the file to get a unique IP address
   break
 
 host = #ip address of the router which is blocked to access the internet
+username = #user
+password = #password
 
-connection = routeros_api.RouterOsApiPool(host,'python','aV9GymDzUZK3VmBcK7sxa7RY4J')
+connection = routeros_api.RouterOsApiPool(host, username ,password)
+
 try: api= connection.get_api()
 except: quit()
 
 list_1 = api.get_resource('/ip/firewall/nat/')
+
 parameters_1 = {'chain':'srcnat','action':'src-nat','to-addresses':'10.101.127.25'}
 parameters_2 = {'to-addresses': x }
+
 try : list_2 = list_1.get(**parameters_1)[0]["id"]
 except: quit()
+
 list_1.set(id=list_2,**parameters_2)
 
 list_2 = api.get_resource('/ip/firewall/connection/')
 list_3 = list_2.get()
 
-search_ip ='75.159.219.7'
+search_ip = host
 search_name ='reply-dst-address'
 search_id = 'id'
 list_id = []
@@ -41,7 +47,7 @@ for index in  range(len(list_3)):
     if search_ip in b and search_name in a:
       list_id.append(list_3[index]['id'])
 
-
+      
 for c  in  list_id:
   parameters_4 = {'id' : c } 
   list_4 = list_2.get(**parameters_4)
