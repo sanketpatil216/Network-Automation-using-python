5eef2b88d4cebd6494dff80b55964c29f52ce3b2
sanketpatil216 <45464462+sanketpatil216@users.noreply.github.com>
2021-04-14 16:52:49 -0600

    Update ip_address_switch.py

--- ip_address_switch.py
+++ ip_address_switch.py
@@ -20,7 +20,7 @@ for line_f1 in f1:                #Comparing the file to get a unique IP address
       continue   
   break
 
-search_ip  #ip address of the router which is blocked to access the internet
+search_ip = #ip address of the router which is blocked to access the internet
 username = #user
 password = #password
 
@@ -31,26 +31,26 @@ except: quit()
 
 list_1 = api.get_resource('/ip/firewall/nat/')
 list_6 = list_1.get()
+
 index1 = 0
 
-for index1 in range(len(list_5)):
+for index1 in range(len(list_5)):           #searching for NAT rules with the old IP address
   for m,n in list_5[index1].iteritems():
     if('to-addresses' in m and search_ip in n):
       list_nat.append(list_5[index1]['id']
                       
-parameters_2 = {'to-addresses': x }
+parameters = {'to-addresses': x }
                       
 for o in nat_list:
-                      list_1.set(id=o, **parameters_2)
-
-
-list_2 = api.get_resource('/ip/firewall/connection/')
+                      list_1.set(id=o, **parameters)    #moving the bloacked IP NAT rules over to the new IP address
+                      
+list_2 = api.get_resource('/ip/firewall/connection/')   #Flush out connections with old IP address
 list_3 = list_2.get()
 
-list_4 =  api.get_resource('/ip/address/')
+list_4 =  api.get_resource('/ip/address/')      #add address in the router
 list_4.add(address =x, interface = bridge1)
 
-list_5 = api.get_resource('/routing/bgp/network/')
+list_5 = api.get_resource('/routing/bgp/network/')    #add address in BGP routing
 list_5.add(network = x, synchronize='no')
 
 search_ip = host
@@ -65,10 +65,9 @@ for index in  range(len(list_3)):
       list_id.append(list_3[index]['id'])
 
       
-for c  in  list_id:
-  parameters_4 = {'id' : c } 
-  list_4 = list_2.get(**parameters_4)
-
+for c  in  list_id:       #flush connections
+  try: list_2.remove(id=c)
+  except: continue
 
 f2.write(line_f1)
 f2.close()
