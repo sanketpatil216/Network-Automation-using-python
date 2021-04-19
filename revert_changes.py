37f5fb03786c691c7eeb239dfabd313898ce3689
sanketpatil216 <45464462+sanketpatil216@users.noreply.github.com>
2021-04-19 12:15:51 -0600

    Update revert_changes.py

--- revert_changes.py
+++ revert_changes.py
@@ -5,10 +5,6 @@ import routeros_api
 f2 = open("/home/sanket/used_ip.txt", 'r+b')     #file containing list of address that have already been used
 search_ip = # Original IP that was replaced
 
-for ip in f2.split('\n'):
-  if (ip == search_ip);
-    ip.replace(
-
 search_ip = #ip address of the router which is blocked to access the internet
 username = #user
 password = #password
@@ -18,6 +14,30 @@ connection = routeros_api.RouterOsApiPool(search_ip, username ,password)
 try: api= connection.get_api()
 except: quit()
 
+f2 = open('used_ip.txt','r')
+data = f2.readlines()
+                 
+op = []
+                                      
+for lines in data:
+  try:
+    x = lines.split('\t')[0]
+    y = lines.split('\t')[1]
+    
+    y = y.strip()
+    
+    if y!= search_ip:
+          op.append(a)
+        
+    else:
+          search_ip =x
+                      
+  except: continue
+                         
+f2 = open('used_ip.txt','w')
+
+f2.writelines(op)
+f2.close()
       
 def loop_function(list_pass):
       index = 0
@@ -25,12 +45,12 @@ def loop_function(list_pass):
       list_new = []
       for index in range(len(list)):           #searching for NAT rules with the old IP address
             for m,n in list[index].iteritems():
-                  if(search_ip in n):
+                  if( in n):
                         list_new.append(list[index1]['id']     
                                         
             for o in list_new:
                       list_pass.remove(id=o)    #moving the bloacked IP NAT rules over to the new IP address
-                      
+                                                       
 list_1 = api.get_resource('/ip/firewall/connection/')   #Flush out connections with old IP address
 loop_function(list_1)
 
